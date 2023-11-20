25102aa6fa9087ac85da175363b6226f9bac8c77
John Weaver <john@saebyn.info>
2023-11-19 16:32:33 -0800

    Add frontend service and update dataProvider URL

--- frontend/src/dataProvider.ts
+++ frontend/src/dataProvider.ts
@@ -1,5 +1,5 @@
 import simpleRestDataProvider from "ra-data-simple-rest";
 
 export const dataProvider = simpleRestDataProvider(
-  process.env.API_HOST || "http://localhost:3000/records"
+  `${import.meta.env.VITE_API_URL || "http://localhost:3000"}/records`
 );
