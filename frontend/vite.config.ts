7bf38dc0c9a6d089b0f45df9e94a3906af901549
John Weaver <john@saebyn.info>
2023-11-26 08:35:49 -0800

    Use fixed port for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': process.env,
-    },
-    server: {
-        host: true,
-    },
-    base: './',
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
+  base: "./",
 });
