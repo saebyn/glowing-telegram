40eea1a355fda4cc3cff640d0512db95a6a6ec14
John Weaver <john@saebyn.info>
2023-11-30 09:24:45 -0800

    Fix parse_duration function to handle duration values correctly

--- crud_api/src/handlers/utils.rs
+++ crud_api/src/handlers/utils.rs
@@ -2,11 +2,11 @@ use diesel::data_types::PgInterval;
 
 pub fn parse_duration(duration: Option<String>) -> Result<PgInterval, String> {
     match duration
-        .unwrap_or("P0S".to_string())
+        .unwrap_or("PT0S".to_string())
         .parse::<iso8601::Duration>()
     {
-        Ok(durationValue) => Ok(PgInterval::from_microseconds(
-            core::time::Duration::from(durationValue).as_micros() as i64,
+        Ok(duration_value) => Ok(PgInterval::from_microseconds(
+            core::time::Duration::from(duration_value).as_micros() as i64,
         )),
         Err(e) => {
             tracing::error!("Error parsing duration: {}", e);
