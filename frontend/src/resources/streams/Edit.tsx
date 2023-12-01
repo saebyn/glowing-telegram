1ba7a18c33dc8c2746d4ba282656bd958da46df0
John Weaver <john@saebyn.info>
2023-11-30 16:37:56 -0800

    Refactor StreamEdit component and add StreamVideoClipsInput component

--- frontend/src/resources/streams/Edit.tsx
+++ frontend/src/resources/streams/Edit.tsx
@@ -1,14 +1,13 @@
 import {
-  ArrayInput,
   DateInput,
-  DateTimeInput,
   Edit,
   EditProps,
   ReferenceArrayInput,
   SimpleForm,
-  SimpleFormIterator,
   TextInput,
 } from "react-admin";
+
+import StreamVideoClipsInput from "./StreamVideoClipsInput";
 
 const StreamEdit = (props: EditProps) => (
   <Edit {...props}>
@@ -22,24 +21,7 @@ const StreamEdit = (props: EditProps) => (
         <TextInput source="id" />
       </ReferenceArrayInput>
 
-      <ArrayInput source="video_clips">
-        <SimpleFormIterator inline>
-          <TextInput source="title" required />
-          <TextInput source="uri" />
-          <TextInput source="duration" />
-          <TextInput source="start_time" />
-          <TextInput source="audio_bitrate" />
-          <TextInput source="audio_track_count" />
-          <TextInput source="content_type" />
-          <TextInput source="filename" />
-          <TextInput source="frame_rate" />
-          <TextInput source="height" />
-          <TextInput source="width" />
-          <TextInput source="video_bitrate" />
-          <TextInput source="size" />
-          <DateTimeInput source="last_modified" />
-        </SimpleFormIterator>
-      </ArrayInput>
+      <StreamVideoClipsInput source="video_clips" />
     </SimpleForm>
   </Edit>
 );
