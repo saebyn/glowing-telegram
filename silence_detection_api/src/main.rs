eba7ef1de5c81d0639093ec782a584e0e439945c
John Weaver <john@saebyn.info>
2023-12-10 09:45:42 -0800

    Complete silence segment detection endpoint

--- silence_detection_api/src/main.rs
+++ silence_detection_api/src/main.rs
@@ -1,11 +1,10 @@
 use axum::{
-    extract::{Query, State},
+    extract::State,
     http::{header, StatusCode},
     response::IntoResponse,
     routing::post,
     Json,
 };
-use chrono::format;
 use common_api_lib;
 use dotenvy;
 use regex::Regex;
@@ -17,6 +16,8 @@ use tracing::instrument;
 #[derive(Clone, Debug)]
 struct AppState {
     video_storage_path: String,
+    noise: f64,
+    duration: f64,
 }
 
 #[tokio::main]
@@ -24,6 +25,16 @@ async fn main() -> Result<(), axum::BoxError> {
     let state = AppState {
         video_storage_path: dotenvy::var("VIDEO_STORAGE_PATH")
             .expect("VIDEO_STORAGE_PATH must be set"),
+
+        noise: dotenvy::var("NOISE")
+            .expect("NOISE must be set")
+            .parse::<f64>()
+            .expect("NOISE must be a float"),
+
+        duration: dotenvy::var("DURATION")
+            .expect("DURATION must be set")
+            .parse::<f64>()
+            .expect("DURATION must be a float"),
     };
 
     common_api_lib::run(state, |app| {
@@ -35,9 +46,6 @@ async fn main() -> Result<(), axum::BoxError> {
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Cursor {
-    #[serde(deserialize_with = "common_api_lib::serde::deserialize_duration")]
-    #[serde(serialize_with = "common_api_lib::serde::serialize_duration")]
-    offset: std::time::Duration,
     index: usize,
 }
 
@@ -46,6 +54,9 @@ struct DetectSegmentInput {
     uris: Vec<String>,
     track: u8,
     cursor: Option<Cursor>,
+
+    noise: Option<f64>,
+    duration: Option<f64>,
 }
 
 #[derive(Serialize, Debug)]
@@ -69,19 +80,31 @@ async fn detect_segment(
     State(state): State<AppState>,
     Json(body): Json<DetectSegmentInput>,
 ) -> impl IntoResponse {
-    let noise = 0.0001;
-    let duration = 0.5;
+    let noise = body.noise.unwrap_or(state.noise);
+    let duration = body.duration.unwrap_or(state.duration);
+
+    // if no uris are provided, return an empty list of segments
+    if body.uris.is_empty() {
+        let output = DetectSegmentOutput {
+            cursor: None,
+            segments: Vec::new(),
+        };
+
+        return (StatusCode::OK, axum::Json(json!(output))).into_response();
+    }
 
     let track = body.track;
 
     // get cursor or create a new one
     let cursor = match body.cursor {
         Some(cursor) => cursor,
-        None => Cursor {
-            offset: std::time::Duration::from_secs(0),
-            index: 0,
-        },
+        None => Cursor { index: 0 },
     };
+
+    // if cursor is out of bounds, return an error
+    if cursor.index >= body.uris.len() {
+        return (StatusCode::BAD_REQUEST, "invalid cursor").into_response();
+    }
 
     let uri = body.uris[cursor.index].clone();
     // extract filename from uri
@@ -92,7 +115,7 @@ async fn detect_segment(
 
     let path = format!("{}/{}", state.video_storage_path, filename);
 
-    let output = match Command::new("ffmpeg")
+    let command_output = match Command::new("ffmpeg")
         .arg("-hide_banner")
         .arg("-i")
         .arg(path)
@@ -100,7 +123,7 @@ async fn detect_segment(
         .arg(format!("0:a:{}", track))
         .arg("-af")
         .arg(format!(
-            "silencedetect=noise={},duration={}",
+            "silencedetect=noise={}:duration={}",
             noise, duration
         ))
         .arg("-f")
@@ -113,7 +136,21 @@ async fn detect_segment(
         Err(_) => return (StatusCode::INTERNAL_SERVER_ERROR, "ffmpeg error").into_response(),
     };
 
-    let output = String::from_utf8_lossy(&output.stderr);
+    let command_stderr = String::from_utf8_lossy(&command_output.stderr);
+
+    // handle output status code
+    if !command_output.status.success() {
+        tracing::error!("ffmpeg error: {}", command_stderr);
+        return (StatusCode::INTERNAL_SERVER_ERROR, "ffmpeg error").into_response();
+    }
+
+    // trace output
+    tracing::trace!("ffmpeg output: {}", command_stderr);
+
+    // detect error in filter by looking for "Conversion failed!" in output
+    if command_stderr.contains("Conversion failed!") {
+        return (StatusCode::INTERNAL_SERVER_ERROR, "ffmpeg error").into_response();
+    }
 
     let re = match Regex::new(
         r"silence_end: (?<end>\d+(\.\d+)?) \| silence_duration: (?<duration>\d+(\.\d+)?)",
@@ -124,12 +161,7 @@ async fn detect_segment(
 
     let mut segments = Vec::new();
 
-    // TODO find length of the recording
-    let length = std::time::Duration::from_secs(900);
-
-    // TODO add the offset to the start and end of each segment
-
-    for cap in re.captures_iter(&output) {
+    for cap in re.captures_iter(&command_stderr) {
         let end = cap["end"].parse::<f64>().unwrap();
         let duration = cap["duration"].parse::<f64>().unwrap();
 
@@ -141,11 +173,19 @@ async fn detect_segment(
         });
     }
 
-    // TODO handle case where we are at the end of the list
+    // handle case where we are at the end of the list
+    let length = body.uris.len();
+    if cursor.index + 1 >= length {
+        let output = DetectSegmentOutput {
+            cursor: None,
+            segments,
+        };
+
+        return (StatusCode::OK, axum::Json(json!(output))).into_response();
+    }
 
     let output = DetectSegmentOutput {
         cursor: Some(Cursor {
-            offset: cursor.offset + length,
             index: cursor.index + 1,
         }),
         segments,
@@ -162,5 +202,7 @@ struct DetectInput {
 
 #[instrument]
 async fn detect(Json(_body): Json<DetectInput>) -> impl IntoResponse {
+    // TODO: implement
+
     (StatusCode::ACCEPTED, [(header::LOCATION, "test")]).into_response()
 }
