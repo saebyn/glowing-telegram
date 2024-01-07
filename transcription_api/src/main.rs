226b8ec145d4fa4cbd34672e9a288e2a7caa1d06
John Weaver <john@saebyn.info>
2024-01-07 07:39:36 -0800

    Refactor main.rs file and add new API endpoint for segment detection

--- transcription_api/src/main.rs
+++ transcription_api/src/main.rs
@@ -1,13 +1,6 @@
-use axum::{
-    extract::State,
-    http::{header, status, StatusCode},
-    response::IntoResponse,
-    routing::post,
-    Json,
-};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json};
 use common_api_lib;
 use dotenvy;
-use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::process::Stdio;
@@ -17,6 +10,12 @@ use tracing::instrument;
 #[derive(Clone, Debug)]
 struct AppState {
     video_storage_path: String,
+
+    task_api_url: String,
+
+    this_api_base_url: String,
+
+    http_client: reqwest::Client,
 }
 
 #[tokio::main]
@@ -24,6 +23,13 @@ async fn main() -> Result<(), axum::BoxError> {
     let state = AppState {
         video_storage_path: dotenvy::var("VIDEO_STORAGE_PATH")
             .expect("VIDEO_STORAGE_PATH must be set"),
+
+        task_api_url: dotenvy::var("TASK_API_URL").expect("TASK_API_URL must be set"),
+
+        this_api_base_url: dotenvy::var("THIS_API_BASE_URL")
+            .expect("THIS_API_BASE_URL must be set"),
+
+        http_client: reqwest::Client::new(),
     };
 
     common_api_lib::run(state, |app| {
@@ -43,16 +49,59 @@ struct DetectSegmentInput {
     uris: Vec<String>,
     track: u8,
     cursor: Option<Cursor>,
+    language: Option<String>,
+    initial_prompt: Option<String>,
+}
+
+/**
+ * An individual segment of a transcript with a start and end duration in ISO 8601 format, and
+ * the text of the segment.
+ */
+#[derive(Serialize, Debug)]
+struct Segment {
+    #[serde(serialize_with = "common_api_lib::serde::serialize_duration")]
+    start: std::time::Duration,
+    #[serde(serialize_with = "common_api_lib::serde::serialize_duration")]
+    end: std::time::Duration,
+    text: String,
 }
 
 #[instrument]
-async fn detect_segment(Json(_body): Json<DetectSegmentInput>) -> impl IntoResponse {
+async fn detect_segment(
+    State(state): State<AppState>,
+    Json(body): Json<DetectSegmentInput>,
+) -> impl IntoResponse {
+    let language = match body.language {
+        Some(language) => language,
+        None => "en".to_string(),
+    };
+
+    let initial_prompt = match body.initial_prompt {
+        Some(initial_prompt) => initial_prompt,
+        None => "".to_string(),
+    };
+
+    let cursor = match body.cursor {
+        Some(cursor) => cursor,
+        None => Cursor { index: 0 },
+    };
+
+    let uri = &body.uris[cursor.index];
+
+    // extract filename from uri
+    let filename = match uri.split('/').last() {
+        Some(filename) => filename,
+        None => return (StatusCode::BAD_REQUEST, "invalid uri").into_response(),
+    };
+
+    let path = format!("{}/{}", state.video_storage_path, filename);
+
     let mut audio_extraction = match Command::new("ffmpeg")
         .arg("-hide_banner")
         .arg("-i")
-        .arg("/obs/2023-12-31 09-06-10.mkv")
+        .arg(path)
         .arg("-map")
-        .arg("0:a:2")
+        .arg(format!("0:a:{}", body.track))
         .arg("-acodec")
         .arg("pcm_s16le")
         .arg("-ac")
@@ -86,22 +135,35 @@ async fn detect_segment(Json(_body): Json<DetectSegmentInput>) -> impl IntoRespo
         }
     };
 
+    // make a temp dir for the transcription
+    let temp_dir = match tempfile::tempdir() {
+        Ok(temp_dir) => temp_dir,
+        Err(e) => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                axum::Json(json!({ "error": e.to_string() })),
+            )
+                .into_response()
+        }
+    };
+
     let mut whisper_detection = match Command::new("whisper")
         .arg("--model")
         .arg("tiny")
+        .arg("--initial_prompt")
+        .arg(initial_prompt)
         .arg("--model_dir")
         .arg("/model/")
         .arg("--output_format")
         .arg("json")
-        // TODO make a temp dir
         .arg("--output_dir")
-        .arg("/obs/transcriptions/")
+        .arg(temp_dir.path())
         .arg("--task")
         .arg("transcribe")
         .arg("--device")
         .arg("cuda")
         .arg("--language")
-        .arg("en")
+        .arg(language)
         .arg("-")
         .stdin(audio)
         .spawn()
@@ -135,7 +197,7 @@ async fn detect_segment(Json(_body): Json<DetectSegmentInput>) -> impl IntoRespo
             .into_response();
     }
 
-    let transcription_path = "/obs/transcriptions/-.json";
+    let transcription_path = temp_dir.path().join("-.json");
 
     // read the file and parse the json
     let transcription_json = match std::fs::read_to_string(transcription_path) {
@@ -161,18 +223,100 @@ async fn detect_segment(Json(_body): Json<DetectSegmentInput>) -> impl IntoRespo
         }
     };
 
-    axum::Json(json!({ "transcription": transcription })).into_response()
+    let segments = match transcription["segments"].as_array() {
+        Some(segments) => segments,
+        None => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                axum::Json(json!({ "error": "invalid json" })),
+            )
+                .into_response()
+        }
+    };
+
+    // convert the segments to a vector of Segment structs
+    let segments = segments
+        .iter()
+        .map(|segment| {
+            let start = match segment["start"].as_f64() {
+                Some(start) => std::time::Duration::from_micros((start * 1_000_000.0) as u64),
+                None => return None,
+            };
+
+            let end = match segment["end"].as_f64() {
+                Some(end) => std::time::Duration::from_micros((end * 1_000_000.0) as u64),
+                None => return None,
+            };
+
+            let text = match segment["text"].as_str() {
+                Some(text) => text,
+                None => return None,
+            };
+
+            Some(Segment {
+                start,
+                end,
+                text: text.to_string(),
+            })
+        })
+        .filter_map(|segment| segment)
+        .collect::<Vec<Segment>>();
+
+    // if this was the last item in the list, then return None for the cursor
+    // return the segments
+    axum::Json(json!(   {
+        "segments": segments,
+        "cursor": if cursor.index + 1 >= body.uris.len() {
+            None
+        } else {
+            Some(Cursor { index: cursor.index + 1 })
+        },
+    }))
+    .into_response()
 }
 
 #[derive(Deserialize, Debug)]
 struct DetectInput {
     uris: Vec<String>,
     track: u8,
+    language: Option<String>,
+    initial_prompt: Option<String>,
 }
 
 #[instrument]
-async fn detect(Json(_body): Json<DetectInput>) -> impl IntoResponse {
-    // TODO: implement
+async fn detect(State(state): State<AppState>, Json(body): Json<DetectInput>) -> impl IntoResponse {
+    // call task api to find the segments in the background asynchrnously,
+    // and return a 202 Accepted response with the URL of the task
 
-    (StatusCode::ACCEPTED, [(header::LOCATION, "test")]).into_response()
+    let language = match body.language {
+        Some(language) => language,
+        None => "en".to_string(),
+    };
+
+    let initial_prompt = match body.initial_prompt {
+        Some(initial_prompt) => initial_prompt,
+        None => "".to_string(),
+    };
+
+    let uris = body.uris.clone();
+
+    let track = body.track;
+
+    let http_client = state.http_client.clone();
+
+    http_client
+        .post(&state.task_api_url)
+        .json(&json!({
+            "url": format!("{}/detect/segment", state.this_api_base_url),
+            "payload": json!({
+                "uris": uris,
+                "track": track,
+                "language": language,
+                "initial_prompt": initial_prompt,
+            }),
+            "data_key": "segments",
+        }))
+        .send()
+        .await
+        .unwrap();
 }
