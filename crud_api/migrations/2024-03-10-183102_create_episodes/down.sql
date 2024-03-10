567881cba94712112fd1f8f615b47ba07a310fdd
saebyn <saebynx@gmail.com>
2024-03-10 16:25:56 -0700

    Add migration files for creating episode tables and triggers

---
+++ crud_api/migrations/2024-03-10-183102_create_episodes/down.sql
@@ -0,0 +1,10 @@
+-- This file should undo anything in `up.sql`
+drop table if exists topic_episodes;
+
+drop table if exists episodes;
+
+drop table if exists topic_series;
+
+drop table if exists series;
+
+drop table if exists topics;
