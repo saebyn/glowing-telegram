a67d73577ecb37d2d98bbfc7f2bb6ae8159634fd
saebyn <saebynx@gmail.com>
2024-03-12 17:27:31 -0700

    Add WithValueUpdate story to DurationField.stories.tsx

--- frontend/src/DurationInput/mui/DurationField.stories.tsx
+++ frontend/src/DurationInput/mui/DurationField.stories.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
+import { useArgs } from "@storybook/preview-api";
 import DurationField from "./DurationField";
 import { fn } from "@storybook/test";
 
@@ -25,5 +27,21 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     value: "PT0S",
+  },
+};
+
+// story that updates the value of the input with the value from onChange
+export const WithValueUpdate: Story = {
+  args: {
+    value: "PT0S",
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs();
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      updateArgs({ value: event.target.value });
+    };
+
+    return <DurationField {...args} value={value} onChange={onChange} />;
   },
 };
