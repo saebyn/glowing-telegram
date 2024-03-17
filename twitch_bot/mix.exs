7eac56f3dc89908060cf2028062631e9b5328c34
saebyn <saebynx@gmail.com>
2024-03-17 12:06:29 -0700

    make a simple twitch chat bot Fixes #9

---
+++ twitch_bot/mix.exs
@@ -0,0 +1,31 @@
+defmodule TwitchBot.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :twitch_bot,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {TwitchBot.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:gun, "~> 2.1"},
+      {:redix, "~> 1.4"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
