9f4cc8c0b55596be62e62d84f0af5385c9a5671f
saebyn <saebynx@gmail.com>
2024-03-24 12:26:13 -0700

    Refactor message parsing logic and add new message types

--- twitch_bot/lib/twitch_bot/message.ex
+++ twitch_bot/lib/twitch_bot/message.ex
@@ -1,5 +1,16 @@
 defmodule TwitchBot.Message do
-  @enforce_keys [:author, :message]
-  defstruct [:author, :message]
+  @enforce_keys [:type, :message]
+  defstruct(author: nil, message: "", type: :unknown, tags: %{}, channel: nil)
   @type t :: %__MODULE__{author: String.t(), message: String.t()}
+
+  @type type ::
+          :privmsg
+          | :notice
+          | :part
+          | :join
+          | :ping
+          | :userstate
+          | :roomstate
+          | :globaluserstate
+          | :unknown
 end
--- twitch_bot/lib/twitch_bot/message_parser.ex
+++ twitch_bot/lib/twitch_bot/message_parser.ex
@@ -1,29 +1,67 @@
 defmodule TwitchBot.MessageParser do
   def parse_message(message) do
-    # TODO consider a pipeline
-    # e.g. message |> split_message |> parse_message
-    # or  {message, %{}} |> parse_tags() |> parse_whatever() |> elem(1)
-    case String.split(message, " ", parts: 5) do
-      [
-        _tags,
-        ":" <> author,
-        "PRIVMSG",
-        _channel,
-        ":" <> message
-      ] ->
-        author = String.replace(author, ~r/!.*/, "")
+    {message,
+     %{
+       :type => :unknown,
+       :message => message
+     }}
+    |> parse_tags()
+    |> parse_author()
+    |> parse_type()
+    |> parse_channel()
+    |> elem(1)
+  end
 
-        {:ok, %TwitchBot.Message{author: author, message: message}}
+  defp parse_tags({message, message_map}) do
+    case Regex.scan(~r/@([^ ]*)/, message) do
+      [] ->
+        {message, message_map}
 
-      _ ->
-        {:unknown, message}
+      [tags] ->
+        tags
+        |> List.first()
+        |> String.split(";")
+        |> Enum.reduce(%{}, fn tag, acc ->
+          [key, value] = String.split(tag, "=")
+          Map.put(acc, String.to_atom(key), value)
+        end)
+        |> then(fn tags ->
+          Map.put(message_map, :tags, tags)
+        end)
+        |> Map.put(:message, String.replace(message, ~r/@([^ ]*)/, ""))
     end
   end
 
-  def is_message?(message) do
-    case parse_message(message) do
-      {:unknown, _} -> false
-      _ -> true
+  defp parse_author({message, message_map}) do
+    case Regex.scan(~r/:(.*)!/, message) do
+      [] ->
+        {message, message_map}
+
+      [[_, author]] ->
+        Map.put(message_map, :author, author)
+        |> Map.put(:message, String.replace(message, ~r/:(.*)!/, ""))
+    end
+  end
+
+  defp parse_type({message, message_map}) do
+    case Regex.scan(~r/PRIVMSG/, message) do
+      [] ->
+        {message, message_map}
+
+      _ ->
+        Map.put(message_map, :type, :privmsg)
+        |> Map.put(:message, String.replace(message, ~r/PRIVMSG/, ""))
+    end
+  end
+
+  defp parse_channel({message, message_map}) do
+    case Regex.scan(~r/#([^ ]*)/, message) do
+      [] ->
+        {message, message_map}
+
+      [[_, channel]] ->
+        Map.put(message_map, :channel, channel)
+        |> Map.put(:message, String.replace(message, ~r/#([^ ]*)/, ""))
     end
   end
 end
