142244791455c658ff8e1e943fa9915d0863a945
saebyn <saebynx@gmail.com>
2024-03-24 12:26:13 -0700

    Refactor message parsing logic and add new message types

--- twitch_bot/test/message_parser_test.exs
+++ twitch_bot/test/message_parser_test.exs
@@ -10,6 +10,19 @@ defmodule MessageParserTest do
     assert MessageParser.parse_message(message) ==
              {:ok,
               %TwitchBot.Message{
+                tags: %{
+                  badge_info: "founder/3",
+                  badges: "founder/0,sub-gifter/1",
+                  client_nonce: "f67031426d5a2985ceb14d1ba5351b49",
+                  color: "#3BC43B",
+                  display_name: "BrainlessSociety",
+                  emotes: "",
+                  tmi_sent_ts: "1711302722640",
+                  turbo: "0",
+                  user_id: "178536615",
+                  user_type: ""
+                },
+                type: :privmsg,
                 author: "brainlesssociety",
                 message: "doctest will run the tests in the documentation"
               }}
