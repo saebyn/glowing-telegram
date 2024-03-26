5594f8dd76ae404e9d24dedb1350cf0e86ab0dd6
saebyn <saebynx@gmail.com>
2024-03-26 15:44:09 -0700

    Remove unused code related to OTIOExporter

--- frontend/src/OTIOExporter/Exporter.tsx
+++ frontend/src/OTIOExporter/Exporter.tsx
@@ -1,10 +1,3 @@
-/**
- * The Exporter component allows the user to export provided
- * episode data to an OTIO file. The user can choose the base
- * directory for the media files and the output file name.
- * The component also provides a download link for the OTIO file.
- */
-
 import { Button, useGetOne, useRecordContext } from "react-admin";
 import DownloadIcon from "@mui/icons-material/Download";
 
