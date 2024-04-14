ca32319e02be56b1d2ae502d47eb936f36fc6712
saebyn <saebynx@gmail.com>
2024-04-14 14:20:33 -0700

    Refactor useTasks hook to sort tasks by last_updated timestamp

--- frontend/src/TasksDrawer/useTasks.ts
+++ frontend/src/TasksDrawer/useTasks.ts
@@ -42,6 +42,10 @@ const useTasks = () => {
     .filter((task: any) =>
       hideViewed ? task.last_updated > lastViewedTaskTimestamp : true
     )
+    /**
+     * Sort tasks by last_updated timestamp in descending order.
+     * If last_updated is undefined, sort by id in descend
+     */
     .sort((a: Task, b: Task) => {
       if (a.last_updated === undefined || b.last_updated === undefined) {
         return b.id - a.id;
