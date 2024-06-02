009b20739164353fffb53098cdf55f918343a910
saebyn <saebynx+github@gmail.com>
2024-06-02 10:01:31 -0700

    handle and surface openai api errors #89

--- ai_api/src/handlers/complete_chat.rs
+++ ai_api/src/handlers/complete_chat.rs
@@ -1,9 +1,10 @@
 use axum::extract::State;
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use openai_dive::v1::api::Client;
+use openai_dive::v1::error::APIError;
 use openai_dive::v1::resources::chat::{
-    ChatCompletionParameters, ChatCompletionResponseFormat, ChatCompletionResponseFormatType,
-    ChatMessage, ChatMessageContent, Role,
+    ChatCompletionParameters, ChatCompletionResponse, ChatCompletionResponseFormat,
+    ChatCompletionResponseFormatType, ChatMessage, ChatMessageContent, Role,
 };
 use serde::{Deserialize, Serialize};
 
@@ -15,14 +16,56 @@ pub async fn handler(
     // as JSON into a list of `ChatMessage` type records
     Json(payload): Json<Vec<SimpleChatMessage>>,
 ) -> impl IntoResponse {
-    let client = Client::new(state.openai_key());
+    let client = Client::new(state.openai_key.expose_secret().to_string());
 
-    let parameters = ChatCompletionParameters {
-        model: state.openai_model(),
+    let response = match client
+        .chat()
+        .create(build_parameters(&payload, &state.openai_model))
+        .await
+    {
+        Ok(response) => response,
+        Err(e) => {
+            tracing::error!("Failed to complete chat: {:?}", e);
+            match e {
+                APIError::InvalidRequestError(message) => {
+                    return (StatusCode::BAD_REQUEST, Json(message)).into_response();
+                }
+                _ => {
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json("Internal server error"),
+                    )
+                        .into_response();
+                }
+            }
+        }
+    };
+
+    // take the first choice and convert it into a `SimpleChatMessage`
+    // take the original payload and add message to the end
+    let mut payload = payload;
+    payload.push(SimpleChatMessage::from(response));
+
+    // this will be converted into a JSON response
+    // with a status code of `200 OK`
+    (StatusCode::OK, Json(payload)).into_response()
+}
+
+// the input to our `complete_chat` handler
+#[derive(Deserialize, Serialize, Debug)]
+pub struct SimpleChatMessage {
+    content: String,
+    // TODO how do we make this require specific values?
+    role: String,
+}
+
+fn build_parameters(messages: &[SimpleChatMessage], model: &str) -> ChatCompletionParameters {
+    ChatCompletionParameters {
+        model: model.to_string(),
         response_format: Some(ChatCompletionResponseFormat {
             r#type: ChatCompletionResponseFormatType::JsonObject,
         }),
-        messages: payload
+        messages: messages
             .iter()
             .map(|m| {
                 // Role::from_str does not exist, so we have to do this
@@ -42,49 +85,27 @@ pub async fn handler(
             })
             .collect(),
         ..Default::default()
-    };
-
-    let response = client
-        .chat()
-        .create(parameters)
-        .await
-        .expect("failed to complete chat");
-
-    // log the whole response
-    tracing::info!("{:?}", response);
-
-    // take the first choice and convert it into a `SimpleChatMessage`
-    let message = SimpleChatMessage {
-        content: match &response.choices[0].message.content {
-            ChatMessageContent::Text(text) => text.to_string(),
-            _ => "No text content".to_string(),
-        },
-        role: response.choices[0].message.role.to_string().to_lowercase(),
-    };
-
-    // if there is more content to be generated, we will get it here
-    match &response.choices[0].finish_reason {
-        Some(reason) => {
-            tracing::info!("Finish reason: {:?}", reason);
-        }
-        None => {
-            tracing::info!("No finish reason provided");
-        }
     }
-
-    // take the original payload and add message to the end
-    let mut payload = payload;
-    payload.push(message);
-
-    // this will be converted into a JSON response
-    // with a status code of `200 OK`
-    (StatusCode::OK, Json(payload))
 }
 
-// the input to our `complete_chat` handler
-#[derive(Deserialize, Serialize, Debug)]
-pub struct SimpleChatMessage {
-    content: String,
-    // TODO how do we make this require specific values?
-    role: String,
+impl From<ChatCompletionResponse> for SimpleChatMessage {
+    fn from(response: ChatCompletionResponse) -> Self {
+        // TODO if there is more content to be generated, we will get it here
+        match &response.choices[0].finish_reason {
+            Some(reason) => {
+                tracing::info!("Finish reason: {:?}", reason);
+            }
+            None => {
+                tracing::info!("No finish reason provided");
+            }
+        }
+
+        Self {
+            content: match &response.choices[0].message.content {
+                ChatMessageContent::Text(text) => text.to_string(),
+                _ => "No text content".to_string(),
+            },
+            role: response.choices[0].message.role.to_string().to_lowercase(),
+        }
+    }
 }
--- ai_api/src/state.rs
+++ ai_api/src/state.rs
@@ -1,22 +1,21 @@
+use redact::Secret;
+
 #[derive(Clone)]
 pub struct AppState {
-    openai_key: String,
-    openai_model: String,
+    pub openai_key: Secret<String>,
+    pub openai_model: String,
 }
 
 impl AppState {
-    pub fn new(openai_key: String, openai_model: String) -> Self {
+    pub fn new(openai_key_path: String, openai_model: String) -> Self {
         Self {
-            openai_key,
+            openai_key: Secret::new(
+                std::fs::read_to_string(openai_key_path)
+                    .expect("failed to read openai key from OPENAI_KEY_PATH")
+                    .trim()
+                    .to_string(),
+            ),
             openai_model,
         }
-    }
-
-    pub fn openai_key(&self) -> String {
-        self.openai_key.to_string()
-    }
-
-    pub fn openai_model(&self) -> String {
-        self.openai_model.to_string()
     }
 }
