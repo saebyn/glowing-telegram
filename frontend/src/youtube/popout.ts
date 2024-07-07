f6164182a79b0f6a535f19f56615c1cdd6a99f1d
saebyn <saebynx+github@gmail.com>
2024-07-07 07:37:44 -0700

    Wrap yt login popout with DOMContentLoaded

--- frontend/src/youtube/popout.ts
+++ frontend/src/youtube/popout.ts
@@ -1,13 +1,14 @@
-const proceedButton = document.getElementById("proceed-button");
+document.addEventListener("DOMContentLoaded", () => {
+  const proceedButton = document.getElementById("proceed-button");
 
-if (!proceedButton) {
-  throw new Error("Proceed button not found");
-}
-proceedButton.addEventListener("click", () => {
-  // Redirect to YouTube login @ https://accounts.google.com/o/oauth2/auth
-  // but forward all query parameters from this URL to the YouTube login URL
-  const queryParams = new URLSearchParams(window.location.search);
-  const baseUrl = import.meta.env.VITE_YOUTUBE_AUTH_URI;
-  const redirectUrl = `${baseUrl}?${queryParams.toString()}`;
-  window.location.href = redirectUrl;
+  if (!proceedButton) {
+    throw new Error("Proceed button not found");
+  }
+  proceedButton.addEventListener("click", () => {
+    // Forward all query parameters from this URL to the YouTube login URL
+    const queryParams = new URLSearchParams(window.location.search);
+    const baseUrl = import.meta.env.VITE_YOUTUBE_AUTH_URI;
+    const redirectUrl = `${baseUrl}?${queryParams.toString()}`;
+    window.location.href = redirectUrl;
+  });
 });
