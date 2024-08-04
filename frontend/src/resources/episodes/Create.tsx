fd76d0b5f97a03ab398adbcff068821b5469c91e
saebyn <saebynx+github@gmail.com>
2024-08-04 11:15:32 -0700

    chore: Update Create.tsx to include stream_id and series_id inputs

--- frontend/src/resources/episodes/Create.tsx
+++ frontend/src/resources/episodes/Create.tsx
@@ -6,19 +6,34 @@ import {
   ArrayInput,
   BooleanInput,
   SimpleFormIterator,
+  ReferenceInput,
+  SelectInput,
 } from "react-admin";
+import { DurationInput } from "../../DurationInput";
 import YouTubeCategoryInput from "../../YouTubeCategoryInput";
 
 const EpisodeCreate = (props: CreateProps) => (
-  <Create {...props} title="Create a Stream">
+  <Create {...props} title="Create an Episode">
     <SimpleForm>
       <TextInput source="title" required />
 
+      <TextInput source="stream_id" isRequired={true} />
+
+      <ReferenceInput source="series_id" reference="series">
+        <SelectInput optionText="title" />
+      </ReferenceInput>
       <BooleanInput source="notify_subscribers" />
       <YouTubeCategoryInput source="category" />
       <ArrayInput source="tags">
         <SimpleFormIterator>
           <TextInput source="" />
+        </SimpleFormIterator>
+      </ArrayInput>
+
+      <ArrayInput source="tracks">
+        <SimpleFormIterator>
+          <DurationInput source="start" />
+          <DurationInput source="end" />
         </SimpleFormIterator>
       </ArrayInput>
     </SimpleForm>
