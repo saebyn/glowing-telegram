2cd0ab9c9a8b3e8398cdbe2d3716c6f061b88f7c
saebyn <saebynx+github@gmail.com>
2024-12-24 13:18:03 -0800

    Refactor filter expression building in CRUD API to support multiple values and improve readability

--- crud_api/src/dynamodb.rs
+++ crud_api/src/dynamodb.rs
@@ -299,7 +299,7 @@ pub async fn create(
     // Loop over the results and check for any unprocessed items
     // and remove them from the list of item IDs
     if let Some(unprocessed_items) = result.unprocessed_items() {
-        for (_, items) in unprocessed_items {
+        for items in unprocessed_items.values() {
             for item in items {
                 if let Some(put_request) = &item.put_request {
                     if let Some(AttributeValue::S(id)) =
@@ -552,20 +552,34 @@ fn build_filter_expressions(
         };
 
         let attribute_name = format!("#k{i}");
-        let attribute_value = format!(":v{i}");
 
-        filter_expressions.push(if op == "contains" {
-            format!("contains({attribute_name}, {attribute_value})")
-        } else {
-            format!("{attribute_name} {op} {attribute_value}")
-        });
+        let values = match value {
+            serde_json::Value::Array(values) => values,
+            _ => &vec![value.clone()],
+        };
 
-        expression_attribute_names
-            .insert(attribute_name.clone(), base_key.to_string());
-        expression_attribute_values.insert(
-            attribute_value,
-            convert_json_to_attribute_value(value.clone()),
-        );
+        let inner_filter_expr = values
+            .iter()
+            .map(|value| {
+                let attribute_value = format!(":v{i}");
+
+                expression_attribute_names
+                    .insert(attribute_name.clone(), base_key.to_string());
+                expression_attribute_values.insert(
+                    attribute_value.clone(),
+                    convert_json_to_attribute_value(value.clone()),
+                );
+
+                if op == "contains" {
+                    format!("contains({attribute_name}, {attribute_value})")
+                } else {
+                    format!("{attribute_name} {op} {attribute_value}")
+                }
+            })
+            .collect::<Vec<String>>()
+            .join(" OR ");
+
+        filter_expressions.push(format!("({inner_filter_expr})"));
     }
 
     (
