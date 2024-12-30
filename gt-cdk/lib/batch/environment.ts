30d28ab2dbd85ce9e0497942b30d53ad49b21936
saebyn <saebynx+github@gmail.com>
2024-12-29 17:59:35 -0800

    Add BatchEnvironmentConstruct with Fargate and Spot compute environments

--- gt-cdk/lib/batch/environment.ts
+++ gt-cdk/lib/batch/environment.ts
@@ -1,20 +1,69 @@
-import * as cdk from 'aws-cdk-lib';
+import type * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
-import type * as batch from 'aws-cdk-lib/aws-batch';
-import type * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import type * as s3 from 'aws-cdk-lib/aws-s3';
-import type * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
-
-interface XConstructProps {}
+import * as batch from 'aws-cdk-lib/aws-batch';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 /**
  * AWS Batch environment construct
  */
-export default class XConstruct extends Construct {
-  constructor(scope: Construct, id: string, props: XConstructProps) {
+export default class BatchEnvironmentConstruct extends Construct {
+  cpuJobQueue: batch.IJobQueue;
+  gpuJobQueue: cdk.aws_batch.JobQueue;
+
+  constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // TODO
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true });
+
+    const sg = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      allowAllOutbound: true,
+    });
+
+    const fargateComputeEnvironment = new batch.FargateComputeEnvironment(
+      this,
+      'ComputeEnvironment',
+      {
+        vpc,
+        securityGroups: [sg],
+        vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+        maxvCpus: 16,
+      },
+    );
+
+    const spotComputeEnvironment = new batch.ManagedEc2EcsComputeEnvironment(
+      this,
+      'SpotComputeEnvironment',
+      {
+        vpc,
+        securityGroups: [sg],
+        vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+        minvCpus: 0,
+        maxvCpus: 16,
+        instanceTypes: [new ec2.InstanceType('g4dn')],
+        spot: true,
+        allocationStrategy:
+          batch.AllocationStrategy.SPOT_PRICE_CAPACITY_OPTIMIZED,
+      },
+    );
+
+    this.cpuJobQueue = new batch.JobQueue(this, 'JobQueue', {
+      computeEnvironments: [
+        {
+          computeEnvironment: fargateComputeEnvironment,
+          order: 1,
+        },
+      ],
+    });
+
+    this.gpuJobQueue = new batch.JobQueue(this, 'GPUJobQueue', {
+      computeEnvironments: [
+        {
+          computeEnvironment: spotComputeEnvironment,
+          order: 1,
+        },
+      ],
+    });
   }
 }
