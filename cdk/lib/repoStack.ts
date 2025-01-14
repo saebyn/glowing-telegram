c6868301caac7d28d4e351e44d777b4ef9c83ea0
saebyn <saebynx+github@gmail.com>
2025-01-13 19:30:03 -0800

    Add S3 bucket and GitHub OIDC role for frontend asset management

--- cdk/lib/repoStack.ts
+++ cdk/lib/repoStack.ts
@@ -1,8 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import type { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import RepoConstruct from './util/repoConstruct';
 
 export default class RepoStack extends cdk.Stack {
+  public readonly frontendAssetBucket: s3.IBucket;
+  githubRole: iam.IRole;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -16,6 +21,42 @@ export default class RepoStack extends cdk.Stack {
         'video-ingestor',
         'twitch-lambda',
       ],
+    });
+
+    this.frontendAssetBucket = new s3.Bucket(this, 'FrontendAssetBucket', {
+      versioned: false,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    const audience = 'sts.amazonaws.com';
+    const githubOrg = 'saebyn';
+    const githubRepo = 'glowing-telegram-frontend';
+
+    const provider = new iam.OpenIdConnectProvider(this, 'GithubOIDCProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: ['sts.amazonaws.com'],
+      thumbprints: ['ffffffffffffffffffffffffffffffffffffffff'],
+    });
+
+    const principal = new iam.OpenIdConnectPrincipal(provider, {
+      StringEquals: {
+        'token.actions.githubusercontent.com:sub': `repo:${githubOrg}/${githubRepo}`,
+        'token.actions.githubusercontent.com:aud': audience,
+      },
+    });
+
+    this.githubRole = new iam.Role(this, 'GithubActionRole', {
+      assumedBy: principal,
+      inlinePolicies: {
+        GithubActionPolicy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              actions: ['s3:PutObject'],
+              resources: [this.frontendAssetBucket.arnForObjects('*')],
+            }),
+          ],
+        }),
+      },
     });
   }
 }
