b958ce2e2d57c45b29a6abaefc7a0e9fc12d4053
saebyn <saebynx+github@gmail.com>
2025-02-03 17:16:38 -0800

    Add handling of refresh tokens

--- twitch_lambda/src/main.rs
+++ twitch_lambda/src/main.rs
@@ -1,20 +1,22 @@
-use aws_config::{meta::region::RegionProviderChain, BehaviorVersion};
-use aws_sdk_secretsmanager::client::Client as SecretsManagerClient;
+use aws_config::{BehaviorVersion, meta::region::RegionProviderChain};
+use aws_sdk_secretsmanager::{
+    client::Client as SecretsManagerClient,
+    types::{FilterNameStringType, builders::FilterBuilder},
+};
 use axum::{
-    async_trait,
+    Json, Router, async_trait,
     body::Body,
     extract::{FromRequestParts, State},
-    http::{header, Request, StatusCode},
+    http::{Request, StatusCode, header},
     response::IntoResponse,
     routing::{get, post},
-    Json, Router,
 };
 use figment::Figment;
-use lambda_http::{tower, RequestExt};
-use oauth2::{AuthorizationCode, CsrfToken, Scope};
+use lambda_http::{RequestExt, tower};
+use oauth2::{AuthorizationCode, CsrfToken, Scope, TokenResponse};
 use serde::Deserialize;
 use serde_json::json;
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 use tower_http::{compression::CompressionLayer, trace::TraceLayer};
 use tracing::instrument;
 use types::{
@@ -22,6 +24,7 @@ use types::{
     TwitchCallbackRequest, TwitchCallbackResponse,
 };
 
+mod secret;
 mod twitch;
 
 #[derive(Debug, Clone, Deserialize)]
@@ -137,6 +140,10 @@ async fn main() {
             "/auth/twitch/token",
             get(obtain_twitch_access_token_handler),
         )
+        .route(
+            "/internal/refresh_user_tokens",
+            post(refresh_user_tokens_handler),
+        )
         .fallback(|| async {
             (
                 StatusCode::NOT_FOUND,
@@ -176,14 +183,14 @@ async fn obtain_twitch_authorization_url_handler(
         .url();
 
     // store csrf_state and redirect_url in secrets manager
-    match update_secret_fields(
+    match secret::create_or_replace(
         &state.secrets_manager,
         &state.config.user_secret_path.secret_path(&cognito_user_id),
-        HashMap::from_iter(vec![
-            ("csrf_state".to_string(), csrf_state.secret().to_string()),
-            ("redirect_url".to_string(), request.redirect_uri.clone()),
-            ("scopes".to_string(), request.scopes.join(",")),
-        ]),
+        &secret::new(
+            csrf_state.secret().to_string(),
+            request.redirect_uri.clone(),
+            request.scopes.clone(),
+        ),
     )
     .await
     {
@@ -192,7 +199,7 @@ async fn obtain_twitch_authorization_url_handler(
             tracing::error!("failed to store in secrets manager: {:?}", e);
             return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
         }
-    }
+    };
 
     let response = json!(AuthorizationUrlResponse {
         url: authorize_url.to_string(),
@@ -270,21 +277,19 @@ async fn twitch_callback_handler(
         }
     };
 
-    let fields = HashMap::from_iter(vec![
-        (
-            "access_token".to_string(),
-            token_response.access_token.secret().to_string(),
-        ),
-        (
-            "refresh_token".to_string(),
-            token_response
-                .refresh_token
-                .map_or(String::new(), |t| t.secret().to_string()),
-        ),
-    ]);
-
-    match update_secret_fields(&state.secrets_manager, &secret_id, fields)
-        .await
+    match secret::set_tokens(
+        &state.secrets_manager,
+        &secret_id,
+        token_response.access_token.secret(),
+        token_response
+            .refresh_token
+            .as_ref()
+            .map_or("", |t| t.secret()),
+        token_response.expires_in().map_or(0.0, |d| {
+            d.as_secs_f64() + chrono::Utc::now().timestamp() as f64
+        }),
+    )
+    .await
     {
         Ok(()) => (),
         Err(e) => {
@@ -318,13 +323,7 @@ async fn obtain_twitch_access_token_handler(
     let secret_id =
         state.config.user_secret_path.secret_path(&cognito_user_id);
 
-    let secret = match state
-        .secrets_manager
-        .get_secret_value()
-        .secret_id(&secret_id)
-        .send()
-        .await
-    {
+    let secret = match secret::get(&state.secrets_manager, &secret_id).await {
         Ok(secret) => secret,
         Err(e) => {
             tracing::error!("failed to get secret: {:?}", e);
@@ -332,35 +331,16 @@ async fn obtain_twitch_access_token_handler(
         }
     };
 
-    let secret_json_object = match serde_json::from_str::<serde_json::Value>(
-        secret.secret_string.as_deref().unwrap_or("{}"),
-    ) {
-        Ok(secret_string) => secret_string,
-        Err(e) => {
-            tracing::error!("failed to parse secret string: {:?}", e);
-            return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
-        }
-    };
-
-    // TODO check if the token is expired and refresh it
-    let access_token = secret_json_object
-        .get("access_token")
-        .and_then(|t| t.as_str());
-
-    if access_token.is_none() {
+    let Some(access_token) = secret.access_token else {
         tracing::warn!("access_token not found in secret");
         return (StatusCode::UNAUTHORIZED,).into_response();
-    }
+    };
 
     if let Ok(validation_response) =
-        twitch::validate_token(access_token.unwrap()).await
+        twitch::validate_token(&access_token).await
     {
         let response_body = AccessTokenResponse {
-            access_token: secret_json_object
-                .get("access_token")
-                .map_or("", |t| t.as_str().unwrap_or(""))
-                .to_string(),
-
+            access_token,
             broadcaster_id: validation_response.user_id,
         };
 
@@ -372,24 +352,89 @@ async fn obtain_twitch_access_token_handler(
             .into_response();
     }
 
-    // if the token is invalid, return unauthorized
+    // if the token is invalid, try to refresh it
     tracing::warn!("invalid access token");
 
-    // clear the access token and refresh token from the secrets manager secret
-    match state
-        .secrets_manager
-        .put_secret_value()
-        .secret_id(&secret_id)
-        .secret_string("{}")
-        .send()
-        .await
-    {
-        Ok(_) => (StatusCode::UNAUTHORIZED,).into_response(),
+    let Some(refresh_token) = secret.refresh_token else {
+        tracing::warn!("refresh_token not found in secret");
+        return (StatusCode::UNAUTHORIZED,).into_response();
+    };
+
+    let client = match twitch::get_oauth_client(&state.twitch_credentials) {
+        Ok(client) => client,
         Err(e) => {
-            tracing::error!("failed to clear access token: {:?}", e);
+            tracing::error!("failed to get Twitch OAuth client: {:?}", e);
             return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
         }
-    }
+    };
+
+    let token_response = match client
+        .exchange_refresh_token(&oauth2::RefreshToken::new(refresh_token))
+        .request_async(oauth2::reqwest::async_http_client)
+        .await
+    {
+        Ok(token) => token,
+        Err(e) => {
+            // clear the access token and refresh token from the secrets manager secret
+
+            tracing::error!("failed to refresh token: {:?}", e);
+            return match secret::clear_tokens(
+                &state.secrets_manager,
+                &secret_id,
+            )
+            .await
+            {
+                Ok(_) => (StatusCode::UNAUTHORIZED,).into_response(),
+                Err(e) => {
+                    tracing::error!("failed to clear access token: {:?}", e);
+                    return (StatusCode::INTERNAL_SERVER_ERROR,)
+                        .into_response();
+                }
+            };
+        }
+    };
+
+    match secret::set_tokens(
+        &state.secrets_manager,
+        &secret_id,
+        token_response.access_token.secret(),
+        token_response
+            .refresh_token
+            .as_ref()
+            .map_or("", |t| t.secret()),
+        token_response.expires_in().map_or(0.0, |d| {
+            d.as_secs_f64() + chrono::Utc::now().timestamp() as f64
+        }),
+    )
+    .await
+    {
+        Ok(()) => {
+            if let Ok(validation_response) =
+                twitch::validate_token(&access_token).await
+            {
+                let response_body = AccessTokenResponse {
+                    access_token: token_response
+                        .access_token
+                        .secret()
+                        .to_string(),
+                    broadcaster_id: validation_response.user_id,
+                };
+
+                return (
+                    StatusCode::OK,
+                    [(header::CONTENT_TYPE, "application/json")],
+                    Json(json!(response_body)),
+                )
+                    .into_response();
+            }
+            tracing::error!("failed to validate refreshed token");
+            return (StatusCode::UNAUTHORIZED,).into_response();
+        }
+        Err(e) => {
+            tracing::error!("failed to store in secrets manager: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+        }
+    };
 }
 
 // axum extractor to get cognito user id from the request
@@ -424,68 +469,130 @@ where
     }
 }
 
-async fn update_secret_fields(
-    secrets_manager: &SecretsManagerClient,
-    secret_id: &str,
-    fields: HashMap<String, String>,
-) -> Result<(), String> {
-    let fetched_secret_string = match secrets_manager
-        .get_secret_value()
-        .secret_id(secret_id)
-        .send()
-        .await
-    {
-        Ok(secret) => secret.secret_string.unwrap_or_else(|| "{}".to_string()),
-        Err(e) => {
-            let e_message = e.to_string();
-            tracing::error!("failed to get secret: {:?}", e);
-            // if the secret doesn't exist, create it
-            if e.into_service_error().is_resource_not_found_exception() {
-                secrets_manager
-                    .create_secret()
-                    .name(secret_id)
-                    .secret_string("{}")
-                    .send()
-                    .await
-                    .map_err(|e| {
-                        tracing::error!("failed to create secret: {:?}", e);
-                        e.to_string()
-                    })?;
-
-                "{}".to_string()
-            } else {
-                return Err(e_message);
+/// Refresh the user's Twitch access token using the refresh token
+/// stored in the secrets manager.
+/// This endpoint is intended to be called internally via an `EventBridge`
+/// scheduled event to keep all users' tokens up to date.
+/// The user's Twitch tokens are stored in the secrets manager
+/// under the path `user_secret_path`.
+async fn refresh_user_tokens_handler(
+    State(state): State<AppState>,
+) -> impl IntoResponse {
+    // iterate over all the secrets in the secrets manager under the
+    // `user_secret_path` prefix, iterating while there are more pages
+    // of secrets to fetch.
+    let mut next_token = None;
+    loop {
+        let list_secrets_response = match state
+            .secrets_manager
+            .list_secrets()
+            .set_next_token(next_token)
+            .filters(
+                FilterBuilder::default()
+                    .key(FilterNameStringType::Name)
+                    .values(state.config.user_secret_path.0.clone())
+                    .build(),
+            )
+            .send()
+            .await
+        {
+            Ok(response) => response,
+            Err(e) => {
+                tracing::error!("failed to list secrets: {:?}", e);
+                return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
             }
-        }
-    };
+        };
 
-    let mut parsed_secret = match serde_json::from_str::<serde_json::Value>(
-        &fetched_secret_string,
-    ) {
-        Ok(secret_string) => secret_string,
-        Err(e) => {
-            tracing::error!("failed to parse secret string: {:?}", e);
-            serde_json::Value::Object(serde_json::Map::new())
-        }
-    };
+        let secrets_list = match list_secrets_response.secret_list {
+            Some(secrets) => secrets,
+            None => {
+                tracing::info!("no secrets found");
+                break;
+            }
+        };
 
-    for (key, value) in fields {
-        parsed_secret[key] = serde_json::Value::String(value);
+        for secret in secrets_list {
+            let secret_id = secret.name.as_deref().unwrap_or("");
+            let secret =
+                match secret::get(&state.secrets_manager, secret_id).await {
+                    Ok(secret) => secret,
+                    Err(e) => {
+                        tracing::error!("failed to get secret: {:?}", e);
+                        continue;
+                    }
+                };
+
+            let Some(access_token) = secret.access_token else {
+                tracing::warn!("access_token not found in secret");
+                continue;
+            };
+
+            let Some(refresh_token) = secret.refresh_token else {
+                tracing::warn!("refresh_token not found in secret");
+                continue;
+            };
+
+            // first, use the validation endpoint to check if the access token is still valid
+            // this also satisfies the requirement to check the token every hour
+            // (assuming that this function is called at least once an hour)
+            if let Ok(_) = twitch::validate_token(&access_token).await {
+                continue;
+            }
+
+            // if the token is invalid, try to refresh it
+            match do_refresh(&state, secret_id, refresh_token).await {
+                Ok(()) => (),
+                Err(e) => {
+                    tracing::error!(
+                        "failed to store in secrets manager: {:?}",
+                        e
+                    );
+                    continue;
+                }
+            };
+        }
+
+        next_token = list_secrets_response.next_token;
+        if next_token.is_none() {
+            break;
+        }
+
+        // sleep for a short time to avoid throttling
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+        tracing::info!("fetching next page of secrets");
     }
 
-    secrets_manager
-        .put_secret_value()
-        .secret_id(secret_id)
-        .secret_string(
-            serde_json::to_string(&parsed_secret)
-                .map_err(|e| e.to_string())?,
-        )
-        .send()
+    (StatusCode::OK,).into_response()
+}
+
+async fn do_refresh(
+    state: &AppState,
+    secret_id: &str,
+    refresh_token: String,
+) -> Result<(), String> {
+    let client = twitch::get_oauth_client(&state.twitch_credentials)
+        .map_err(|e| e.to_string())?;
+
+    let token_response = client
+        .exchange_refresh_token(&oauth2::RefreshToken::new(refresh_token))
+        .request_async(oauth2::reqwest::async_http_client)
         .await
-        .map_err(|e| {
-            tracing::error!("failed to update secret: {:?}", e);
-            e.to_string()
-        })?;
+        .map_err(|e| e.to_string())?;
+
+    secret::set_tokens(
+        &state.secrets_manager,
+        secret_id,
+        token_response.access_token.secret(),
+        token_response
+            .refresh_token
+            .as_ref()
+            .map_or("", |t| t.secret()),
+        token_response.expires_in().map_or(0.0, |d| {
+            d.as_secs_f64() + chrono::Utc::now().timestamp() as f64
+        }),
+    )
+    .await?;
 
     Ok(())
 }
