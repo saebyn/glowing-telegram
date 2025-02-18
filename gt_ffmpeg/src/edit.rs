0b46a46f196d91960939d835505569cd6bf9aa46
saebyn <saebynx+github@gmail.com>
2025-02-17 16:08:09 -0800

    refactor(gt_ffmpeg): absract filter graph for edit mod

--- gt_ffmpeg/src/edit.rs
+++ gt_ffmpeg/src/edit.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Display};
+use std::{collections::HashMap, rc::Rc};
 use tokio::process::Command;
 
 use types::{CutList, InputMedia, OutputTrack, TransitionInType};
@@ -7,28 +9,158 @@ fn convert_frame_to_time(frame: i64, frame_rate: f32) -> f32 {
     frame as f32 / frame_rate
 }
 
-fn create_complex_filter(filter_steps: &[String]) -> String {
-    filter_steps.join(";")
+/// Represents a filter with a name and a set of options.
+struct Filter {
+    name: Rc<str>,
+    options: HashMap<Rc<str>, Rc<str>>,
 }
 
-fn data_to_filter_complex(
-    data: &CutList,
-    frame_rate: f32,
-    resolution: (u32, u32),
-) -> String {
-    let mut filter_steps = Vec::new();
-    // 1) main track building
-    filter_steps.extend(create_main_track(data, frame_rate, resolution));
-    // 2) overlay tracks
-    filter_steps.extend(create_overlay_tracks(data, frame_rate));
-    create_complex_filter(&filter_steps)
+impl Filter {
+    fn new(name: &str, options: Vec<(&str, std::string::String)>) -> Self {
+        Self {
+            name: name.into(),
+            options: options
+                .into_iter()
+                .map(|(k, v)| (k.into(), v.into()))
+                .collect(),
+        }
+    }
+}
+
+impl Display for Filter {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut parts: Vec<String> = self
+            .options
+            .iter()
+            .map(|(k, v)| {
+                if v.is_empty() {
+                    k.to_string()
+                } else {
+                    format!("{}={}", k, v)
+                }
+            })
+            .collect();
+
+        parts.sort();
+
+        write!(f, "{}={}", self.name, parts.join(":"))
+    }
+}
+
+/// FilterChannel represents an input or output of a filter.
+/// It is represented as a tuple of (channel_type, index, is_source).
+/// channel_type is 'v' for video and 'a' for audio.
+/// This corresponds to the ffmpeg filter complex syntax, such that
+/// [0:v] is the first video stream that is input to the filter complex,
+/// and [v0] is the first video stream that is output from the filter complex.
+/// These are represented as FilterChannel::SourceVideo(0) and
+/// FilterChannel::MyVideo(0) respectively.
+/// These could be constructed using the new method, but there are also
+/// convenience methods for video and audio streams, such as
+/// FilterChannel::SourceVideo(0) and FilterChannel::MyVideo(0).
+enum FilterChannel {
+    SourceAudio(u8),
+    SourceVideo(u8),
+    MyAudio(u8),
+    MyVideo(u8),
+    OverlayVideo(u8),
+}
+
+impl Display for FilterChannel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            FilterChannel::SourceAudio(index) => write!(f, "[{}:a]", index),
+            FilterChannel::SourceVideo(index) => write!(f, "[{}:v]", index),
+            FilterChannel::MyAudio(index) => write!(f, "[a{}]", index),
+            FilterChannel::MyVideo(index) => write!(f, "[v{}]", index),
+            FilterChannel::OverlayVideo(index) => write!(f, "[o{}]", index),
+        }
+    }
+}
+
+/// Represents a filter pipe with a set of filters and inputs.
+struct FilterPipe {
+    filters: Rc<[Filter]>,
+    inputs: Rc<[FilterChannel]>,
+    output: FilterChannel,
+}
+
+impl FilterPipe {
+    fn new(
+        filters: Vec<Filter>,
+        inputs: Vec<FilterChannel>,
+        output: FilterChannel,
+    ) -> Self {
+        Self {
+            filters: filters.into(),
+            inputs: inputs.into(),
+            output,
+        }
+    }
+}
+
+impl Display for FilterPipe {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}{}{}",
+            self.inputs
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join(""),
+            self.filters
+                .iter()
+                .map(|filter| filter.to_string())
+                .collect::<Vec<_>>()
+                .join(","),
+            self.output,
+        )
+    }
+}
+
+/// Represents a filter graph with a set of pipes.
+struct FilterGraph {
+    pipes: Rc<[FilterPipe]>,
+}
+
+impl FilterGraph {
+    fn new(
+        cutlist: &CutList,
+        frame_rate: f32,
+        resolution: (u32, u32),
+    ) -> Self {
+        let mut pipes = Vec::new();
+        // 1) main track building
+        pipes.extend(create_main_track(cutlist, frame_rate, resolution));
+        // 2) overlay tracks
+        pipes.extend(create_overlay_tracks(cutlist, frame_rate, resolution));
+
+        Self {
+            pipes: pipes.into(),
+        }
+    }
+}
+
+impl Display for FilterGraph {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.pipes
+                .iter()
+                .map(|pipe| pipe.to_string())
+                .collect::<Vec<_>>()
+                .join(";")
+        )
+    }
 }
 
 fn create_main_track(
     data: &CutList,
     frame_rate: f32,
     resolution: (u32, u32),
-) -> Vec<String> {
+) -> Vec<FilterPipe> {
     let mut parts = Vec::new();
     for (i, track) in data.output_track.iter().enumerate() {
         let media_item = &data.input_media[track.media_index as usize];
@@ -66,16 +198,22 @@ fn add_video_to_output_track(
     output_track_index: usize,
     frame_rate: f32,
     resolution: (u32, u32),
-) -> String {
-    format!(
-        "[{media_index}:v]scale=w={width}:h={height},trim=start_frame={start_frame}:end_frame={end_frame},setpts=PTS-STARTPTS,fps=fps={frame_rate}[v{output_track_index}]",
-        media_index = media_index,
-        start_frame = start_frame,
-        end_frame = end_frame,
-        frame_rate = frame_rate,
-        output_track_index = output_track_index,
-        width = resolution.0,
-        height = resolution.1
+) -> FilterPipe {
+    FilterPipe::new(
+        vec![
+            Filter::new("scale", vec![
+                ("w", resolution.0.to_string()),
+                ("h", resolution.1.to_string()),
+            ]),
+            Filter::new("trim", vec![
+                ("start_frame", start_frame.to_string()),
+                ("end_frame", end_frame.to_string()),
+            ]),
+            Filter::new("setpts", vec![("PTS-STARTPTS", "".to_string())]),
+            Filter::new("fps", vec![("fps", frame_rate.to_string())]),
+        ],
+        vec![FilterChannel::SourceVideo(media_index as u8)],
+        FilterChannel::MyVideo(output_track_index as u8),
     )
 }
 
@@ -85,15 +223,20 @@ fn add_audio_to_output_track(
     end_frame: i64,
     output_track_index: usize,
     frame_rate: f32,
-) -> String {
+) -> FilterPipe {
     let start_sec = convert_frame_to_time(start_frame, frame_rate);
     let end_sec = convert_frame_to_time(end_frame, frame_rate);
-    format!(
-        "[{media_index}:a]atrim=start={start_sec}:end={end_sec},asetpts=PTS-STARTPTS[a{output_track_index}]",
-        media_index = media_index,
-        start_sec = start_sec,
-        end_sec = end_sec,
-        output_track_index = output_track_index
+
+    FilterPipe::new(
+        vec![
+            Filter::new("atrim", vec![
+                ("start", start_sec.to_string()),
+                ("end", end_sec.to_string()),
+            ]),
+            Filter::new("asetpts", vec![("PTS-STARTPTS", "".to_string())]),
+        ],
+        vec![FilterChannel::SourceAudio(media_index as u8)],
+        FilterChannel::MyAudio(output_track_index as u8),
     )
 }
 
@@ -101,7 +244,7 @@ fn assemble_track_segments(
     output_tracks: &[OutputTrack],
     input_media: &[InputMedia],
     frame_rate: f32,
-) -> Vec<String> {
+) -> Vec<FilterPipe> {
     let mut parts = Vec::new();
     let mut video_streams = Vec::new();
     let mut audio_streams = Vec::new();
@@ -121,46 +264,71 @@ fn assemble_track_segments(
                 );
                 let offset = prev_len_sec - duration_sec;
                 // xfade
-                parts.push(format!(
-                    "[v{0}][v{1}]xfade=duration={duration_sec}:transition=fade:offset={offset}[v{0}]",
-                    i - 1,
-                    i
+                parts.push(FilterPipe::new(
+                    vec![Filter::new("xfade", vec![
+                        ("duration", duration_sec.to_string()),
+                        ("transition", "fade".to_string()),
+                        ("offset", offset.to_string()),
+                    ])],
+                    vec![
+                        FilterChannel::MyVideo((i - 1) as u8),
+                        FilterChannel::MyVideo(i as u8),
+                    ],
+                    FilterChannel::MyVideo((i - 1) as u8),
                 ));
-                parts.push(format!(
-                    "[a{0}][a{1}]acrossfade=d={duration_sec}[a{0}]",
-                    i - 1,
-                    i
+
+                parts.push(FilterPipe::new(
+                    vec![Filter::new("acrossfade", vec![(
+                        "d",
+                        duration_sec.to_string(),
+                    )])],
+                    vec![
+                        FilterChannel::MyAudio((i - 1) as u8),
+                        FilterChannel::MyAudio(i as u8),
+                    ],
+                    FilterChannel::MyAudio((i - 1) as u8),
                 ));
             } else {
-                video_streams.push(format!("v{}", i));
-                audio_streams.push(format!("a{}", i));
+                video_streams.push(FilterChannel::MyVideo(i as u8));
+                audio_streams.push(FilterChannel::MyAudio(i as u8));
             }
         } else {
-            video_streams.push(format!("v{}", i));
-            audio_streams.push(format!("a{}", i));
+            video_streams.push(FilterChannel::MyVideo(i as u8));
+            audio_streams.push(FilterChannel::MyAudio(i as u8));
         }
     }
 
     // Concat final streams
     if !video_streams.is_empty() {
-        parts.push(format!(
-            "[{}]concat=n={}[vmain]",
-            video_streams.join("]["),
-            video_streams.len()
+        parts.push(FilterPipe::new(
+            vec![Filter::new("concat", vec![(
+                "n",
+                video_streams.len().to_string(),
+            )])],
+            video_streams,
+            FilterChannel::MyVideo(255),
         ));
     }
     if !audio_streams.is_empty() {
-        parts.push(format!(
-            "[{}]concat=n={}:v=0:a=1[amain]",
-            audio_streams.join("]["),
-            audio_streams.len()
+        parts.push(FilterPipe::new(
+            vec![Filter::new("concat", vec![
+                ("n", audio_streams.len().to_string()),
+                ("v", "0".to_string()),
+                ("a", "1".to_string()),
+            ])],
+            audio_streams,
+            FilterChannel::MyAudio(255),
         ));
     }
 
     parts
 }
 
-fn create_overlay_tracks(data: &CutList, frame_rate: f32) -> Vec<String> {
+fn create_overlay_tracks(
+    data: &CutList,
+    frame_rate: f32,
+    resolution: (u32, u32),
+) -> Vec<FilterPipe> {
     let mut parts = Vec::new();
 
     let overlay_tracks = match data.overlay_tracks {
@@ -172,19 +340,57 @@ fn create_overlay_tracks(data: &CutList, frame_rate: f32) -> Vec<String> {
         let media_index = overlay.media_index;
         let section = &data.input_media[media_index as usize].sections
             [overlay.section_index as usize];
-        parts.push(format!(
-            "[{}:v]trim=start_frame={}:end_frame={},colorkey=black,colorchannelmixer=aa=0.8,setpts=PTS+{}/TB,format=yuva420p[vo{}]",
-            media_index,
-            section.start_frame,
-            section.end_frame,
-            convert_frame_to_time(overlay.start_frame, frame_rate),
-            i
+
+        let mut filters = Vec::new();
+
+        if overlay.overlay_track_type == types::OverlayTrackType::Colorkey {
+            filters.push(Filter::new("colorkey", vec![(
+                "black",
+                "".to_string(),
+            )]));
+            filters.push(Filter::new("colorchannelmixer", vec![(
+                "aa",
+                "0.8".to_string(),
+            )]));
+        }
+
+        filters.extend(vec![
+            Filter::new("trim", vec![
+                ("start_frame", section.start_frame.to_string()),
+                ("end_frame", section.end_frame.to_string()),
+            ]),
+            Filter::new("scale", vec![
+                ("w", resolution.0.to_string()),
+                ("h", resolution.1.to_string()),
+            ]),
+            Filter::new("setpts", vec![(
+                format!(
+                    "PTS+{}/TB",
+                    convert_frame_to_time(overlay.start_frame, frame_rate)
+                )
+                .as_str(),
+                "".to_string(),
+            )]),
+            Filter::new("format", vec![("yuva420p", "".to_string())]),
+        ]);
+
+        parts.push(FilterPipe::new(
+            filters,
+            vec![FilterChannel::SourceVideo(media_index as u8)],
+            FilterChannel::OverlayVideo(i as u8),
         ));
-        parts.push(format!(
-            "[vmain][vo{}]overlay=eof_action=pass:x={}:y={}[vmain]",
-            i,
-            overlay.x.unwrap_or(0.0),
-            overlay.y.unwrap_or(0.0)
+
+        parts.push(FilterPipe::new(
+            vec![Filter::new("overlay", vec![
+                ("eof_action", "pass".to_string()),
+                ("x", overlay.x.unwrap_or(0.0).to_string()),
+                ("y", overlay.y.unwrap_or(0.0).to_string()),
+            ])],
+            vec![
+                FilterChannel::MyVideo(255),
+                FilterChannel::OverlayVideo(i as u8),
+            ],
+            FilterChannel::MyVideo(255),
         ));
     }
     parts
@@ -203,11 +409,11 @@ pub fn build_ffmpeg_command(
         cmd.arg("-i").arg(m.s3_location.clone());
     }
 
-    let filter = data_to_filter_complex(data, frame_rate, resolution);
+    let filter = FilterGraph::new(data, frame_rate, resolution).to_string();
 
     cmd.arg("-filter_complex").arg(filter);
 
-    cmd.arg("-map").arg("[vmain]").arg("-map").arg("[amain]");
+    cmd.arg("-map").arg("[v255]").arg("-map").arg("[a255]");
     cmd.arg("-y")
         .arg("-c:v")
         .arg("libx264")
@@ -237,19 +443,7 @@ mod test {
     use serde_json::json;
     use types::CutList;
 
-    use super::{build_ffmpeg_command, create_complex_filter};
-
-    #[test]
-    fn test_create_complex_filter() {
-        let filter = create_complex_filter(&[
-            "scale=w=2560:h=1440:trim=start_frame=0:end_frame=100".to_string(),
-            "atrim=start=0:end=1".to_string(),
-        ]);
-        assert_eq!(
-            filter,
-            "scale=w=2560:h=1440:trim=start_frame=0:end_frame=100;atrim=start=0:end=1"
-        );
-    }
+    use super::build_ffmpeg_command;
 
     #[test]
     fn test_real_example_1() {
@@ -359,21 +553,20 @@ mod test {
                  {
                   "mediaIndex": 5,
                   "sectionIndex": 0,
-                  "startFrame": 0
+                  "startFrame": 0,
+                  "type": "alpha"
                  },
                  {
                   "mediaIndex": 6,
                   "sectionIndex": 0,
                   "startFrame": 1800,
-                  "x": 1500,
-                  "y": 300
+                  "type": "colorkey"
                  },
                  {
                   "mediaIndex": 7,
                   "sectionIndex": 0,
                   "startFrame": 3600,
-                  "x": 1500,
-                  "y": 300
+                  "type": "colorkey"
                  }
                 ],
                 "version": "1.0.0"
@@ -391,7 +584,7 @@ mod test {
                 .map(|x| x.to_string_lossy().to_string())
                 .collect::<Vec<_>>()
                 .join(" "),
-            "-i 2024-12-08/2024-12-08 08-26-45.mkv -i 2024-12-08/2024-12-08 08-46-51.mkv -i 2024-12-08/2024-12-08 09-06-51.mkv -i 2024-12-08/2024-12-08 09-26-51.mkv -i my_stock/outro_2.mov -i my_stock/introhex.mkv -i my_stock/LiveOnTwitch Render 1.mov -i my_stock/LikeReminder1 Render 1.mov -filter_complex [0:v]scale=w=2560:h=1440,trim=start_frame=20100:end_frame=72250,setpts=PTS-STARTPTS,fps=fps=60[v0];[0:a]atrim=start=335:end=1204.1666,asetpts=PTS-STARTPTS[a0];[1:v]scale=w=2560:h=1440,trim=start_frame=0:end_frame=72000,setpts=PTS-STARTPTS,fps=fps=60[v1];[1:a]atrim=start=0:end=1200,asetpts=PTS-STARTPTS[a1];[2:v]scale=w=2560:h=1440,trim=start_frame=0:end_frame=72000,setpts=PTS-STARTPTS,fps=fps=60[v2];[2:a]atrim=start=0:end=1200,asetpts=PTS-STARTPTS[a2];[3:v]scale=w=2560:h=1440,trim=start_frame=0:end_frame=8326,setpts=PTS-STARTPTS,fps=fps=60[v3];[3:a]atrim=start=0:end=138.76666,asetpts=PTS-STARTPTS[a3];[4:v]scale=w=2560:h=1440,trim=start_frame=0:end_frame=1800,setpts=PTS-STARTPTS,fps=fps=60[v4];[4:a]atrim=start=0:end=30,asetpts=PTS-STARTPTS[a4];[v3][v4]xfade=duration=5:transition=fade:offset=133.76666[v3];[a3][a4]acrossfade=d=5[a3];[v0][v1][v2][v3]concat=n=4[vmain];[a0][a1][a2][a3]concat=n=4:v=0:a=1[amain];[5:v]trim=start_frame=0:end_frame=3600,colorkey=black,colorchannelmixer=aa=0.8,setpts=PTS+0/TB,format=yuva420p[vo0];[vmain][vo0]overlay=eof_action=pass:x=0:y=0[vmain];[6:v]trim=start_frame=0:end_frame=114,colorkey=black,colorchannelmixer=aa=0.8,setpts=PTS+30/TB,format=yuva420p[vo1];[vmain][vo1]overlay=eof_action=pass:x=1500:y=300[vmain];[7:v]trim=start_frame=0:end_frame=300,colorkey=black,colorchannelmixer=aa=0.8,setpts=PTS+60/TB,format=yuva420p[vo2];[vmain][vo2]overlay=eof_action=pass:x=1500:y=300[vmain] -map [vmain] -map [amain] -y -c:v libx264 -c:a aac -crf 18 -preset slow -pix_fmt yuv420p -profile:v high -level 4.2 -bf 2 -g 120 -b:a 192k -ar 48000 -f mp4 output.mp4"
+            "-i 2024-12-08/2024-12-08 08-26-45.mkv -i 2024-12-08/2024-12-08 08-46-51.mkv -i 2024-12-08/2024-12-08 09-06-51.mkv -i 2024-12-08/2024-12-08 09-26-51.mkv -i my_stock/outro_2.mov -i my_stock/introhex.mkv -i my_stock/LiveOnTwitch Render 1.mov -i my_stock/LikeReminder1 Render 1.mov -filter_complex [0:v]scale=h=1440:w=2560,trim=end_frame=72250:start_frame=20100,setpts=PTS-STARTPTS,fps=fps=60[v0];[0:a]atrim=end=1204.1666:start=335,asetpts=PTS-STARTPTS[a0];[1:v]scale=h=1440:w=2560,trim=end_frame=72000:start_frame=0,setpts=PTS-STARTPTS,fps=fps=60[v1];[1:a]atrim=end=1200:start=0,asetpts=PTS-STARTPTS[a1];[2:v]scale=h=1440:w=2560,trim=end_frame=72000:start_frame=0,setpts=PTS-STARTPTS,fps=fps=60[v2];[2:a]atrim=end=1200:start=0,asetpts=PTS-STARTPTS[a2];[3:v]scale=h=1440:w=2560,trim=end_frame=8326:start_frame=0,setpts=PTS-STARTPTS,fps=fps=60[v3];[3:a]atrim=end=138.76666:start=0,asetpts=PTS-STARTPTS[a3];[4:v]scale=h=1440:w=2560,trim=end_frame=1800:start_frame=0,setpts=PTS-STARTPTS,fps=fps=60[v4];[4:a]atrim=end=30:start=0,asetpts=PTS-STARTPTS[a4];[v3][v4]xfade=duration=5:offset=133.76666:transition=fade[v3];[a3][a4]acrossfade=d=5[a3];[v0][v1][v2][v3]concat=n=4[v255];[a0][a1][a2][a3]concat=a=1:n=4:v=0[a255];[5:v]trim=end_frame=3600:start_frame=0,scale=h=1440:w=2560,setpts=PTS+0/TB,format=yuva420p[o0];[v255][o0]overlay=eof_action=pass:x=0:y=0[v255];[6:v]colorkey=black,colorchannelmixer=aa=0.8,trim=end_frame=114:start_frame=0,scale=h=1440:w=2560,setpts=PTS+30/TB,format=yuva420p[o1];[v255][o1]overlay=eof_action=pass:x=0:y=0[v255];[7:v]colorkey=black,colorchannelmixer=aa=0.8,trim=end_frame=300:start_frame=0,scale=h=1440:w=2560,setpts=PTS+60/TB,format=yuva420p[o2];[v255][o2]overlay=eof_action=pass:x=0:y=0[v255] -map [v255] -map [a255] -y -c:v libx264 -c:a aac -crf 18 -preset slow -pix_fmt yuv420p -profile:v high -level 4.2 -bf 2 -g 120 -b:a 192k -ar 48000 -f mp4 output.mp4"
         );
     }
 }
