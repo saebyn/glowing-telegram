0c18f75362f8126df8b7ae95f521aa8aab3d5d6f
saebyn <saebynx+github@gmail.com>
2025-02-22 18:30:27 -0800

    feat(youtube_lambda): add initial implementation for YouTube OAuth Lambda and configuration

---
+++ youtube_lambda/src/handlers.rs
@@ -0,0 +1,180 @@
+use axum::{
+    Json,
+    extract::State,
+    http::{StatusCode, header},
+    response::IntoResponse,
+};
+use gt_axum::cognito::CognitoUserId;
+use oauth2::{AuthorizationCode, CsrfToken, Scope, TokenResponse};
+use serde_json::json;
+use tracing::instrument;
+use types::{
+    AccessTokenResponse, AuthorizationUrlResponse, YouTubeAuthRequest,
+    YouTubeCallbackRequest, YouTubeCallbackResponse, YouTubeSessionSecret,
+};
+
+use crate::{structs::AppState, youtube};
+
+#[instrument(skip(state))]
+pub async fn obtain_youtube_authorization_url_handler(
+    State(state): State<AppState>,
+    CognitoUserId(cognito_user_id): CognitoUserId,
+    Json(request): Json<YouTubeAuthRequest>,
+) -> impl IntoResponse {
+    let Ok(client) = youtube::get_oauth_client(&state.youtube_credentials)
+    else {
+        return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+    };
+
+    let (authorize_url, csrf_state) = client
+        .authorize_url(CsrfToken::new_random)
+        .add_scopes(
+            request.scopes.iter().map(|scope| Scope::new(scope.clone())),
+        )
+        .url();
+
+    // store csrf_state and redirect_url in secrets manager
+    match gt_secrets::create_or_replace::<YouTubeSessionSecret>(
+        &state.secrets_manager,
+        &state.config.user_secret_path.secret_path(&cognito_user_id),
+        &gt_secrets::SessionSecret::new(
+            csrf_state.secret().to_string(),
+            request.redirect_uri.clone(),
+            request.scopes.clone(),
+        ),
+    )
+    .await
+    {
+        Ok(()) => (),
+        Err(e) => {
+            tracing::error!("failed to store in secrets manager: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+        }
+    };
+
+    let response = json!(AuthorizationUrlResponse {
+        url: authorize_url.to_string(),
+    });
+
+    (
+        StatusCode::OK,
+        [(header::CONTENT_TYPE, "application/json")],
+        Json(response),
+    )
+        .into_response()
+}
+
+#[instrument(skip(state))]
+pub async fn youtube_callback_handler(
+    State(state): State<AppState>,
+    CognitoUserId(cognito_user_id): CognitoUserId,
+    Json(request): Json<YouTubeCallbackRequest>,
+) -> impl IntoResponse {
+    let secret_id =
+        state.config.user_secret_path.secret_path(&cognito_user_id);
+
+    let Ok(secret) = gt_secrets::get::<YouTubeSessionSecret>(
+        &state.secrets_manager,
+        &secret_id,
+    )
+    .await
+    else {
+        return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+    };
+
+    if secret.csrf_token != request.state {
+        tracing::error!("csrf_state mismatch");
+        return (StatusCode::UNAUTHORIZED,).into_response();
+    }
+
+    let client = match youtube::get_oauth_client(&state.youtube_credentials) {
+        Ok(client) => client,
+        Err(e) => {
+            tracing::error!("failed to get YouTube OAuth client: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+        }
+    };
+
+    let token_response = match client
+        .exchange_code(AuthorizationCode::new(request.code.clone()))
+        .request_async(oauth2::reqwest::async_http_client)
+        .await
+    {
+        Ok(token) => token,
+        Err(e) => {
+            tracing::error!("failed to exchange code: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+        }
+    };
+
+    match gt_secrets::set_tokens::<YouTubeSessionSecret>(
+        &state.secrets_manager,
+        &secret_id,
+        token_response.access_token().secret(),
+        token_response
+            .refresh_token()
+            .as_ref()
+            .map_or("", |t| t.secret()),
+        token_response.expires_in(),
+    )
+    .await
+    {
+        Ok(()) => {
+            tracing::info!("Tokens stored successfully in secrets manager");
+        }
+        Err(e) => {
+            tracing::error!("failed to store in secrets manager: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+        }
+    };
+
+    let response_body = YouTubeCallbackResponse {
+        url: secret.redirect_url,
+    };
+
+    (
+        StatusCode::OK,
+        [(header::CONTENT_TYPE, "application/json")],
+        Json(json!(response_body)),
+    )
+        .into_response()
+}
+
+#[instrument(skip(state))]
+pub async fn obtain_youtube_access_token_handler(
+    State(state): State<AppState>,
+    CognitoUserId(cognito_user_id): CognitoUserId,
+) -> impl IntoResponse {
+    let secret_id =
+        state.config.user_secret_path.secret_path(&cognito_user_id);
+
+    let secret = match gt_secrets::get::<YouTubeSessionSecret>(
+        &state.secrets_manager,
+        &secret_id,
+    )
+    .await
+    {
+        Ok(secret) => secret,
+        Err(e) => {
+            tracing::error!("failed to get secret: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR,).into_response();
+        }
+    };
+
+    let Some(access_token) = secret.access_token else {
+        tracing::warn!("access_token not found in secret");
+        return (StatusCode::UNAUTHORIZED,).into_response();
+    };
+
+    let response_body = AccessTokenResponse {
+        access_token,
+        broadcaster_id: String::new(),
+    };
+
+    (
+        StatusCode::OK,
+        [(header::CONTENT_TYPE, "application/json")],
+        Json(json!(response_body)),
+    )
+        .into_response()
+}
---
+++ youtube_lambda/src/main.rs
@@ -0,0 +1,128 @@
+use aws_config::{BehaviorVersion, meta::region::RegionProviderChain};
+use aws_sdk_secretsmanager::client::Client as SecretsManagerClient;
+use axum::{
+    Json, Router,
+    body::Body,
+    http::{Request, StatusCode, header},
+    routing::{get, post},
+};
+use lambda_http::tower;
+
+use serde_json::json;
+use std::sync::Arc;
+use structs::AppState;
+use tower_http::{compression::CompressionLayer, trace::TraceLayer};
+
+use crate::structs::YouTubeCredentials;
+
+mod handlers;
+mod structs;
+mod youtube;
+
+#[tokio::main]
+async fn main() {
+    // https://docs.aws.amazon.com/lambda/latest/dg/rust-logging.html
+    tracing_subscriber::fmt()
+        .json()
+        // allow log level to be overridden by RUST_LOG env var
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        // this needs to be set to remove duplicated information in the log.
+        .with_current_span(false)
+        // this needs to be set to false, otherwise ANSI color codes will
+        // show up in a confusing manner in CloudWatch logs.
+        .with_ansi(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        // remove the name of the function from every log entry
+        .with_target(false)
+        .init();
+
+    let config = structs::load_config().expect("failed to load config");
+    let region_provider =
+        RegionProviderChain::default_provider().or_else("us-east-1");
+    let aws_config = aws_config::defaults(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
+
+    let secrets_manager = SecretsManagerClient::new(&aws_config);
+
+    let youtube_credentials = match secrets_manager
+        .get_secret_value()
+        .secret_id(&config.youtube_secret_arn)
+        .send()
+        .await
+    {
+        Ok(secret) => match serde_json::from_str::<YouTubeCredentials>(
+            secret.secret_string.as_deref().unwrap_or("{}"),
+        ) {
+            Ok(credentials) => credentials,
+            Err(e) => {
+                tracing::error!("failed to parse YouTube secret: {:?}", e);
+                return;
+            }
+        },
+        Err(e) => {
+            tracing::error!("failed to get YouTube secret: {:?}", e);
+            return;
+        }
+    };
+
+    // Create a shared state to pass to the handler
+    let state = AppState {
+        secrets_manager: Arc::new(secrets_manager),
+        youtube_credentials,
+        config,
+    };
+
+    initialize_api(state).await;
+}
+
+async fn initialize_api(state: AppState) {
+    // Set up a trace layer
+    let trace_layer = TraceLayer::new_for_http().on_request(
+        |request: &Request<Body>, _: &tracing::Span| {
+            tracing::info!(
+                "received request: {method} {uri}",
+                method = request.method(),
+                uri = request.uri()
+            );
+        },
+    );
+
+    let compression_layer = CompressionLayer::new().gzip(true).deflate(true);
+
+    // Create Axum app
+    let app = Router::new()
+        .route(
+            "/auth/youtube/url",
+            post(handlers::obtain_youtube_authorization_url_handler),
+        )
+        .route(
+            "/auth/youtube/callback",
+            post(handlers::youtube_callback_handler),
+        )
+        .route(
+            "/auth/youtube/token",
+            get(handlers::obtain_youtube_access_token_handler),
+        )
+        .fallback(|| async {
+            (
+                StatusCode::NOT_FOUND,
+                [(header::CONTENT_TYPE, "application/json")],
+                Json(json!({
+                    "message": "not found",
+                })),
+            )
+        })
+        .layer(trace_layer)
+        .layer(compression_layer)
+        .with_state(state);
+
+    // Provide the app to the lambda runtime
+    let app = tower::ServiceBuilder::new()
+        .layer(axum_aws_lambda::LambdaLayer::default().trim_stage())
+        .service(app);
+
+    lambda_http::run(app).await.unwrap();
+}
