fe04c26ff57eb495901457f05dbdbfea33cadf2b
saebyn <saebynx+github@gmail.com>
2025-03-01 13:49:48 -0800

    feat(cdk): update frontend version to 0.2.2

--- cdk/bin/gt-cdk.ts
+++ cdk/bin/gt-cdk.ts
@@ -7,7 +7,7 @@ import FrontendStack from '../lib/frontendStack';
 const app = new cdk.App();
 
 const frontendStack = new FrontendStack(app, 'FrontendStack', {
-  frontendVersion: process.env.FRONTEND_VERSION || '0.2.1',
+  frontendVersion: process.env.FRONTEND_VERSION || '0.2.2',
 });
 
 new RepoStack(app, 'RepoStack', {
